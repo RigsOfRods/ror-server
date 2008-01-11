c21b3fa9b8b3f975b2eadddf52c8e32bbe2e91bf
rorthomas <rorthomas@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-01-11 08:37:13 +0000

    added pdcurses

--- trunk/premake.lua
+++ trunk/premake.lua
@@ -8,13 +8,14 @@ package.files = { matchfiles("*.cpp"), matchfiles("*.c") }
 package.objdir = "obj"
 
 if windows then
-	package.includepaths = { "SocketW/src/", "win32_pthread" }
-	package.libpaths = { "win32_pthread" }
+	package.includepaths = { "SocketW/src/", "win32_pthread", "PDCurses-3.3"}
+	package.libpaths = { "win32_pthread", "PDCurses-3.3/win32"}
 	package.defines = { "__WIN32__", "_CRT_SECURE_NO_WARNINGS" }
-	package.links = { "kernel32", "wsock32", "SocketW", "pthreadVC2", "WSOCK32", "ws2_32"}
+	package.links = { "kernel32", "wsock32", "SocketW", "pthreadVC2", "WSOCK32", "ws2_32", "pdcurses"}
 else
-	package.includepaths = { "SocketW/src/"}
-	package.links = { "SocketW", "pthread" }
+	package.includepaths = { "SocketW/src/", "PDCurses-3.3"}
+	package.libpaths = { "PDCurses-3.3/x11"}
+	package.links = { "SocketW", "pthread", "PDCurses", "XCurses" , "pdcurses"}
 end
 
 package = newpackage()
@@ -26,3 +27,8 @@ if windows then
 end
 package.files = { matchfiles("SocketW/src/*.cxx") }
 package.objdir = "SocketW/obj"
+package = newpackage()
+package.name = "PDCurses"
+package.kind = "lib"
+package.language = "c++"
+package.files = { matchfiles("PDCurses-3.3/*.c") }

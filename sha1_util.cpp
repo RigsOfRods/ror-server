c07e70327dbb3285b59ae5354cdd502dac11ce3a
rorthomas <rorthomas@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-01-24 15:37:12 +0000

    *fixed sha1

---
+++ sha1_util.cpp
@@ -0,0 +1,115 @@
+#include "sha1_util.h"
+
+#include "sha1.h"
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+bool toHex(char *result, unsigned char *data, unsigned int len)
+{
+	char tmp[20];
+	unsigned int i=0;
+	while(i<len)
+	{
+		memset(tmp, 0, 20);
+		unsigned char d = (unsigned char)*data;
+		sprintf(tmp, "%x", d);
+		strcat(result, tmp);
+		data++;
+		i++;
+	}
+	return true;
+}
+
+bool SHA1FromString(char *result, char *source)
+{
+	// init variables
+	char sha1result_bin[20];
+	memset(sha1result_bin, 0, 20);
+	char sha1result_hex[41];
+	memset(sha1result_hex, 0, 41);
+
+	// calculate hash of the filenames
+	sha1( (unsigned char *)source, (int)strlen(source), (unsigned char *)sha1result_bin);
+	bool res = toHex(sha1result_hex, (unsigned char*)sha1result_bin, 20);
+	memcpy(result, sha1result_hex, 40);
+	result[40]=0;
+	return res;
+}
+
+bool SHA1FromString(std::string &result, std::string &sourceStr)
+{
+
+	// init variables
+	char sha1result_bin[20];
+	memset(sha1result_bin, 0, 20);
+	char sha1result_hex[41];
+	memset(sha1result_hex, 0, 41);
+
+	// calculate hash of the filenames
+	char *source = const_cast<char *>(sourceStr.c_str());
+	sha1( (unsigned char *)source, (int)strlen(source), (unsigned char *)sha1result_bin);
+	bool res = toHex(sha1result_hex, (unsigned char*)sha1result_bin, 20);
+	if(res)
+	{
+		sha1result_hex[40]=0;
+		result = std::string(sha1result_hex);
+	}
+	return res;
+}
+
+int getFileHash(char *filename, char *hash)
+{
+	FILE *cfd=fopen(filename, "rb");
+	// anti-hacker :|
+	if (cfd)
+	{
+		// obtain file size:
+		fseek (cfd , 0 , SEEK_END);
+		long lSize = ftell (cfd);
+		rewind (cfd);
+
+		// allocate memory to contain the whole file:
+		char *buffer = (char*) malloc (sizeof(char)*(lSize+1));
+		if (buffer == NULL) return -3;
+		memset(buffer, 0, lSize);
+		// copy the file into the buffer:
+		size_t result = fread (buffer,1,lSize,cfd);
+		if (result != lSize) return -2;
+		// terminate
+		fclose (cfd);
+		buffer[lSize]=0;
+		
+		//LogManager::getSingleton().logMessage("buffer-size: " + StringConverter::toString(lSize));
+		//LogManager::getSingleton().logMessage("buffer: " + String(buffer));
+
+		char sha1result[250];
+		memset(sha1result, 0, 250);
+
+		if(lSize<300)
+			return -4;
+
+		if(!SHA1FromString(sha1result, buffer))
+		{
+			return -1;
+		}
+		//LogManager::getSingleton().logMessage("sha1()="+String(sha1result));
+
+		free (buffer);
+		memcpy(hash, sha1result, 40);
+		hash[41]=0;
+		return 0;
+	}
+	return 1;
+}
+
+bool sha1check()
+{
+	char testStr[255]="The quick brown fox jumps over the lazy dog";
+	char result[255]="";
+	char testvalue[255]="2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
+	SHA1FromString(result, testStr);
+
+	//LogManager::getSingleton().logMessage("SHA1('"+String(testStr)+"') = '"+String(result)+"' ?= ");
+	return !strcmp(result, testvalue);
+}
--- sha1_util.h
+++ sha1_util.h
@@ -1,33 +1,12 @@
-#include "sha1.h"
+#ifndef SHA1UTIL_H_
+#define SHA1UTIL_H_
 
-#include <iostream>
-#include <sstream>
-#include <iomanip>
+#include <string>
 
-bool toHex(char *result, char *data, int len)
-{
-	std::ostringstream hexStream;
-	hexStream << std::setw(2) << std::setfill('0') << std::hex;
-	for(unsigned int i=0;(int)i<len;i++)
-	{
-		hexStream << static_cast<unsigned int>(static_cast<unsigned char>(data[i]));
-	}
-	strcpy(result, hexStream.str().c_str());
-	return true;
-}
+bool toHex(char *result, unsigned char *data, unsigned int len);
+bool SHA1FromString(char *result, char *source);
+bool SHA1FromString(std::string &result, std::string &sourceStr);
+int getFileHash(char *filename, char *hash);
+bool sha1check();
 
-bool SHA1FromString(char *result, char *source)
-{
-	char output[20];
-	memset(output, 0, 20);
-
-	char output_hex[255];
-	memset(output_hex, 0, 255);
-
-	sha1( (unsigned char *)source, (int)strlen(source), (unsigned char *)output);
-	if(!toHex(output_hex, output, 40))
-		return false;
-	strncpy(result, output_hex, 40);
-	result[40]=0;
-	return true;
-}
+#endif

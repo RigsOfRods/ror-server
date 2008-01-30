477eb14ce68ba6c5fbf0ba969a6b01fa1ea17706
Aperion <Aperion@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-01-30 04:48:28 +0000

    Changed if... else...  error checking to a try...catch statement

--- listener.cpp
+++ listener.cpp
@@ -17,6 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "listener.h"
+#include <stdexcept>
+#include <string>
+#include <sstream>
 
 void *s_lsthreadstart(void* vid)
 {
@@ -42,6 +45,7 @@ void Listener::threadstart()
 	//here we start
 	SWInetSocket listSocket;
 	SWBaseSocket::SWBaseError error;
+	
 	//manage the listening socket
 	listSocket.bind(lport, &error);
 	if (error!=SWBaseSocket::ok)
@@ -53,111 +57,94 @@ void Listener::threadstart()
 		return;
 	}
 	listSocket.listen();
+	
 	//await connections
 	logmsgf(LOG_WARN,"Listener ready");
 	while (1)
 	{
 		logmsgf(LOG_DEBUG,"Listener awaiting connections");
 		SWInetSocket *ts=(SWInetSocket *)listSocket.accept(&error);
+
 		logmsgf(LOG_DEBUG,"Listener got a new connection");
+		
 		if (error!=SWBaseSocket::ok) 
 		{
 			logmsgf(LOG_ERROR,"ERROR Listener: %s", error.get_error().c_str());
 			continue;
 		}
+		
 		//receive a magic
 		int type;
 		unsigned int source;
 		unsigned int len;
 		char buffer[256];
-		if (Messaging::receivemessage(ts, &type, &source, &len, buffer, 256))
+		
+		try
 		{
-			logmsgf(LOG_ERROR,"ERROR Listener: receiving first message");
-			ts->disconnect(&error);
-			delete ts;
-			continue;
-		}
-		if (type==MSG2_HELLO)
-		{
+			// this is the start of it all, it all starts with a simple hello
+			if (Messaging::receivemessage(ts, &type, &source, &len, buffer, 256))
+				throw std::runtime_error("ERROR Listener: receiving first message");
+			
+			// make sure our first message is a hello message
+			if (type != MSG2_HELLO)
+				throw std::runtime_error("ERROR Listener: bad version");
+			
+			// send client the which version of rornet the server is running
 			logmsgf(LOG_DEBUG,"Listener sending version");
-			//send the version information back
-			if (Messaging::sendmessage(ts, MSG2_VERSION, 0, (int)strlen(RORNET_VERSION), RORNET_VERSION))
-			{
-				logmsgf(LOG_ERROR,"ERROR Listener: sending version");
-				ts->disconnect(&error);
-				delete ts;
-				continue;
-			}
+			if (Messaging::sendmessage(ts, MSG2_VERSION, 0, strlen(RORNET_VERSION), RORNET_VERSION))
+				throw std::runtime_error("ERROR Listener: sending version");
+			
 			logmsgf(LOG_DEBUG,"Listener sending terrain");
 			//send the terrain information back
-			if (Messaging::sendmessage(ts, MSG2_TERRAIN_RESP, 0, (int)strnlen(SEQUENCER.getTerrainName(),250), SEQUENCER.getTerrainName()))
+			if (Messaging::sendmessage( ts, MSG2_TERRAIN_RESP, 0,
+					strlen( SEQUENCER.getTerrainName()),
+					SEQUENCER.getTerrainName()))
+				throw std::runtime_error("ERROR Listener: sending terrain");
+	
+			if(strncmp(buffer, RORNET_VERSION, strlen(RORNET_VERSION)))
+				throw std::runtime_error("ERROR Listener: bad version");
+			
+			//receive user name
+			if (Messaging::receivemessage(ts, &type, &source, &len, buffer, 256))
 			{
-				logmsgf(LOG_ERROR,"ERROR Listener: sending terrain");
-				ts->disconnect(&error);
-				delete ts;
+				std::stringstream error_msg;
+				error_msg << "ERROR Listener: receiving user name\n"
+					<< "ERROR Listener: got that: "
+					<< type;
+				throw std::runtime_error(error_msg.str());
+			}
+			
+			if (type != MSG2_USER_CREDENTIALS)
+				throw std::runtime_error("Warning Listener: no user name");
+			//create a new client
+			//correct a bit the client name (trucate, validate)
+			if (len>sizeof(user_credentials_t))
 				continue;
-			}
-
-			if(!strncmp(buffer, RORNET_VERSION, strlen(RORNET_VERSION)))
+			logmsgf(LOG_DEBUG,"Listener creating a new client...");
+			
+			user_credentials_t *user = (user_credentials_t *)buffer;
+			if(SEQUENCER.isPasswordProtected())
 			{
-				//receive user name
-				if (Messaging::receivemessage(ts, &type, &source, &len, buffer, 256))
+				logmsgf(LOG_DEBUG,"password login: %s == %s?",
+						SEQUENCER.getServerPasswordHash(),
+						user->password);
+				if(strncmp(SEQUENCER.getServerPasswordHash(), user->password, 40))
 				{
-					logmsgf(LOG_ERROR,"ERROR Listener: receiving user name");
-					logmsgf(LOG_ERROR,"ERROR Listener: got that: %d" , type);
-					ts->disconnect(&error);
-					delete ts;
-					continue;
+					Messaging::sendmessage(ts, MSG2_WRONG_PW, 0, 0, 0);
+					throw std::runtime_error( "ERROR Listener: wrong password" );
 				}
-				if (type==MSG2_USER_CREDENTIALS)
-				{
-					//create a new client
-					//correct a bit the client name (trucate, validate)
-					if (len>sizeof(user_credentials_t))
-						continue;
-					logmsgf(LOG_DEBUG,"Listener creating a new client...");
-					user_credentials_t *user = (user_credentials_t *)buffer;
-					if(SEQUENCER.isPasswordProtected())
-					{
-						logmsgf(LOG_DEBUG,"password login: %s == %s?", SEQUENCER.getServerPasswordHash(), user->password);
-						if(!strncmp(SEQUENCER.getServerPasswordHash(), user->password, 40))
-						{
-							logmsgf(LOG_DEBUG,"user used the correct password, creating client!");
-							SEQUENCER.createClient(ts, user);
-							logmsgf(LOG_DEBUG,"listener returned!");
-						}else
-						{
-							Messaging::sendmessage(ts, MSG2_WRONG_PW, 0, 0, 0);
-							logmsgf(LOG_ERROR,"ERROR Listener: wrong password");
-							ts->disconnect(&error);
-							delete ts;
-							continue;
-						}
-					}else
-					{
-						logmsgf(LOG_DEBUG,"creating client, no password protection, creating client");
-						SEQUENCER.createClient(ts, user);
-						logmsgf(LOG_DEBUG,"listener returned!");
-
-					}
-				}
-				else
-				{
-					logmsgf(LOG_ERROR,"Warning Listener: no user name");
-					ts->disconnect(&error);
-					delete ts;
-				}
-			}
-			else
+				
+				logmsgf(LOG_DEBUG,"user used the correct password, creating client!");
+			}else
 			{
-				logmsgf(LOG_ERROR,"ERROR Listener: bad version");
-				ts->disconnect(&error);
-				delete ts;
+				logmsgf(LOG_DEBUG,"creating client, no password protection, creating client");
 			}
+			SEQUENCER.createClient(ts, user);
+			logmsgf(LOG_DEBUG,"listener returned!");
 		}
-		else
+		catch(std::runtime_error e)
 		{
-			logmsgf(LOG_ERROR,"ERROR Listener: No Hello");
+			logmsgf(LOG_ERROR, e.what());
 			ts->disconnect(&error);
 			delete ts;
 		}

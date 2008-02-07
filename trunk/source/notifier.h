3aa8553e07ce21dfa8f4b00c762239b89a64e3d5
aperion <aperion@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-02-07 18:59:57 +0000

    added documentation

--- trunk/source/notifier.h
+++ trunk/source/notifier.h
@@ -24,39 +24,69 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "sequencer.h"
 
 /**
+ * The notifier class communicated with the master server, it is called from
+ * the sequencer class via Sequencer::notifyRoutine. The loop method is
+ * essentially the main loop for the server.
+ * 
  * @brief notifier class handles communications with the master server
  */
 class Notifier
 {
 private:
-	int lport;
-	int maxclient;
-	char* public_ip;
-	char* server_name;
-	char* terrain_name;
-	char httpresp[65536];
-	char challenge[256];
-	bool exit;
-	bool passprotected;
-	bool wasregistered;
-	bool rconenabled;
-	int servermode;
-	int error_count;
+	int lport;             //!< port to listen to for incoming connections
+	int maxclient;         //!< maximum number of clients this server can support
+	char* public_ip;       //!< public ip address of the server
+	char* server_name;     //!< name of the server
+	char* terrain_name;    //!< name of the terrain
+	char httpresp[65536];  //!< http response from the master server
+	char challenge[256];   //!< ??
+	bool exit;             //!< exit the server
+	bool passprotected;    //!< password protection for logging into this server 
+	bool wasregistered;    //!< is registered with the master server
+	bool rconenabled;      //!< check is the remote console is eneabled
+	int servermode;        //!< Internet or LAN mode 
+	int error_count;       //!< ??
 
-	/// registration call to the master server to register this server
-	bool registerServer();
-	bool sendHearbeat();
+	bool registerServer(); //!< attempt to register with the master server
+	bool sendHearbeat();   //!< send a heart beat message to the master server
 
 public:
-	Notifier(char* pubip, int port, int max_client, char* servname, char* terrname, bool pwprotected, int servermode, bool rconenabled);
+    
+	Notifier(char* pubip,
+	        int port,
+	        int max_client,
+	        char* servname,
+	        char* terrname,
+	        bool pwprotected,
+	        int servermode,
+	        bool rconenabled);
+	
 	~Notifier(void);
+	
+	//! main loop for the server?
 	void loop();
-	int HTTPGET(char* URL);
-	int HTTPPOST(char* URL, char* data);
+	
+	/**
+	 * perform an HTTPGET
+     * @param[in] master server address
+     * @return the amount of data received, if negative an error occured
+	 */
+	int HTTPGET(const char* URL);
 
+    /**
+     * perform an HTTPPOST
+     * @param[in] master server address
+     * @param[in] data the message to send
+     * @return the amount of data received, if negative an error occured
+     */
+	int HTTPPOST(const char* URL, const char* data);
+
+	/**
+	 * @return the respond from the last HTTP{GET,POST} method call
+	 */
 	char *getResponse() { return httpresp; };
 
-	/// call to the master server to unregister this server.
+	//! notify the master server that this server is shutting down 
 	bool unregisterServer();
 };
 #endif

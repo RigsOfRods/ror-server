72d264981be31c59551824448c2cf2cf0746ca71
aperion <aperion@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-04-10 02:20:16 +0000

    fixed a bug in strict_tokenize that cause an infinate loop in some machines

--- trunk/source/utils.cpp
+++ trunk/source/utils.cpp
@@ -2,9 +2,7 @@
 #include "utils.h"
 #include "logger.h"
 #include <string>
-#include <sstream>
 #include <vector>
-#include <stdexcept>
 
 void tokenize(const std::string& str,
 				std::vector<std::string>& tokens,
@@ -34,32 +32,19 @@ void strict_tokenize(const std::string& str,
 				std::vector<std::string>& tokens,
 				const std::string& delimiter)
 {
-	STACKLOG;
-	// Skip delimiter at beginning.
-	std::string::size_type lastPos = str.find(delimiter, 0);
-	if( 0 == lastPos) lastPos = delimiter.length();
-	else lastPos = 0;
-	// Find first "non-delimiter".
-	std::string::size_type pos     = str.find(delimiter, lastPos);
-	while (std::string::npos != pos || std::string::npos != lastPos)
-	{
-		// Found a token, add it to the vector.
-		tokens.push_back(str.substr(lastPos, pos - lastPos));
-		// Skip delimiters.  Note the "not_of"
-		lastPos = str.find(delimiter, pos);
-		if( lastPos == pos && lastPos != std::string::npos )
-		{
-			lastPos += delimiter.length();
-			if( lastPos >= str.length() )
-				lastPos = std::string::npos;
-		}
-		
-		// Find next "non-delimiter"
-		pos = str.find(delimiter, lastPos);
-	}
+	size_t prev_loc = 0, new_loc = str.find( delimiter, prev_loc );
+	
+	do {
+		tokens.push_back( str.substr( prev_loc, new_loc - prev_loc ) );
+		prev_loc = ( new_loc > str.length() ? new_loc : new_loc + delimiter.length() );
+		new_loc = str.find( delimiter, prev_loc );
+	} while( new_loc < str.length() && prev_loc < str.length() );
+	
+	tokens.push_back( str.substr( prev_loc, new_loc - prev_loc ) );
 }
 
 std::string trim(const std::string& str )
 {
+	STACKLOG;
 	return str.substr( str.find_first_not_of(" \t"), str.find_last_not_of(" \t")+1);
 }

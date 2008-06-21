19a1cbee81e8649844584f6171c316afbf788e4e
aperion <aperion@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2008-06-21 03:45:46 +0000

    proper handeling of the NOTIMEOUT and NOSTACKLOG defines in the premake file

--- trunk/premake.lua
+++ trunk/premake.lua
@@ -23,9 +23,9 @@ package.config["Release"].target = "SocketW"
 package.language = "c++"
 package.files = { matchfiles("SocketW/src/*.cxx") }
 package.objdir = "SocketW/obj"
-if windows then
-	package.config["Debug"].defines = { "__WIN32__", "WIN32", "_DEBUG", "_WINDOWS", "_USRDLL", "MYSOCKETW_EXPORTS", "NOTIMEOUT"}
-	package.config["Release"].defines = { "__WIN32__", "WIN32", "NDEBUG", "_WINDOWS", "_USRDLL", "MYSOCKETW_EXPORTS", "NOTIMEOUT", "NOSTACKLOG"}
+if windows then }
+	package.config["Debug"].defines = { "__WIN32__", "WIN32", "_DEBUG", "_WINDOWS", "_USRDLL", "MYSOCKETW_EXPORTS"
+	package.config["Release"].defines = { "__WIN32__", "WIN32", "NDEBUG", "_WINDOWS", "_USRDLL", "MYSOCKETW_EXPORTS" }
 	package.links = { "ws2_32" }
 else
 	package.config["Debug"].defines = { "_DEBUG", "MYSOCKETW_EXPORTS"}
@@ -43,12 +43,12 @@ package.objdir = "source/obj"
 if windows then
 	package.includepaths = { "SocketW/src/", "win32_pthread"}
 	package.libpaths = { "win32_pthread"}
-	package.defines = { "__WIN32__", "_CRT_SECURE_NO_WARNINGS" }
+	package.defines = { "__WIN32__", "_CRT_SECURE_NO_WARNINGS", "NOTIMEOUT", "NOSTACKLOG" }
 	package.config["Debug"].links = { "kernel32", "wsock32", "SocketW_d", "pthreadVC2", "WSOCK32"}
 	package.config["Release"].links = { "kernel32", "wsock32", "SocketW", "pthreadVC2", "WSOCK32"}
 else
 	package.includepaths = { "SocketW/src/"}
---	package.defines = { "NOTIMEOUT" }
+	package.defines = { "NOTIMEOUT", "NOSTACKLOG" }
 	
 	package.config["Debug"].links = { "SocketW_d", "pthread"}
 	package.config["Release"].links = { "SocketW", "pthread"}

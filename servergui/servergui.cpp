00e1c47770671a4d27dc68fafe48ddd400ac6f2d
rorthomas <rorthomas@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2009-05-10 00:35:45 +0000

    serverGUI working :D

--- servergui/servergui.cpp
+++ servergui/servergui.cpp
@@ -55,9 +55,12 @@ public:
 private:
 	MyApp *app;
 	wxButton *startBtn, *stopBtn, *exitBtn;
-	wxTextCtrl *txtConsole;
-	wxRadioBox *smode;
+	wxTextCtrl *txtConsole, *ipaddr, *port, *slots, *passwd, *terrain, *sname;
+	wxComboBox *smode, *logmode;
+	wxNotebook *nbook;
+	wxPanel *settingsPanel, *logPanel;
 	void OnQuit(wxCloseEvent &event);
+	int loglevel;
 
 	void OnBtnStart(wxCommandEvent& event);
 	void OnBtnStop(wxCommandEvent& event);
@@ -119,7 +122,7 @@ void pureLogCallback(int level, std::string msg, std::string msgf)
 
 void MyDialog::logCallback(int level, std::string msg, std::string msgf)
 {
-	//if(level == LOG_STACK) return;
+	if(level < loglevel) return;
 
 	if(level == LOG_INFO)
 		txtConsole->SetDefaultStyle(wxTextAttr(wxColour(0,150,0)));
@@ -131,6 +134,8 @@ void MyDialog::logCallback(int level, std::string msg, std::string msgf)
 		txtConsole->SetDefaultStyle(wxTextAttr(wxColour(180,130,0)));
 	else if(level == LOG_DEBUG)
 		txtConsole->SetDefaultStyle(wxTextAttr(wxColour(180,150,0)));
+	else if(level == LOG_STACK)
+		txtConsole->SetDefaultStyle(wxTextAttr(wxColour(150,150,150)));
 
 
     int lines = 0;
@@ -150,6 +155,7 @@ void MyDialog::logCallback(int level, std::string msg, std::string msgf)
 MyDialog::MyDialog(const wxString& title, MyApp *_app) : wxDialog(NULL, wxID_ANY, title,  wxPoint(100, 100), wxSize(500, 500))
 {
 	app=_app;
+	loglevel=LOG_INFO;
 	dialogInstance = this;
 
 	SetMinSize(wxSize(500,500));
@@ -165,29 +171,99 @@ MyDialog::MyDialog(const wxString& title, MyApp *_app) : wxDialog(NULL, wxID_ANY
 	wxStaticPicture *imagePanel = new wxStaticPicture(this, -1, bm,wxPoint(0, 0), wxSize(500, 100), wxNO_BORDER);
 	mainsizer->Add(imagePanel, 0, wxGROW);
 
-	txtConsole = new wxTextCtrl(this, wxID_ANY, _(""), wxDefaultPosition, wxDefaultSize, wxNO_BORDER|wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH|wxTE_RICH2|wxTE_DONTWRAP);
-	mainsizer->Add(txtConsole, 1, wxGROW);
+	nbook=new wxNotebook(this, wxID_ANY);
+	mainsizer->Add(nbook, 1, wxGROW);
 
-	wxArrayString choices;
-	choices.Add(_("LAN"));
-	choices.Add(_("Internet"));
+	///// settings
+	settingsPanel=new wxPanel(nbook, wxID_ANY);
+	wxSizer *settingsSizer = new wxBoxSizer(wxVERTICAL);
+	wxSizer *hsizer = new wxGridSizer(3, 2, 2, 2);
+	wxStaticText *dText;
 
-	wxSizer *hsizer = new wxBoxSizer(wxHORIZONTAL);
+	// IP
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Server Name:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	sname = new wxTextCtrl(settingsPanel, wxID_ANY, _("Default Server"), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(sname, 0, wxGROW);
 
-	smode = new wxRadioBox(this, wxID_ANY, _("Mode"), wxDefaultPosition, wxDefaultSize, choices, 2, wxRA_SPECIFY_ROWS);
+	// mode
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Mode:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+
+	wxString choices[1];
+	choices[0] = _("LAN");
+	choices[1] = _("Internet");
+	smode = new wxComboBox(settingsPanel, wxID_ANY, _("LAN"), wxDefaultPosition, wxDefaultSize, 2, choices, wxCB_DROPDOWN|wxCB_READONLY);
 	hsizer->Add(smode, 0, wxGROW);
 
-	startBtn = new wxButton(this, btn_start, _("START"));
+	// IP
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("IP:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	ipaddr = new wxTextCtrl(settingsPanel, wxID_ANY, _("0.0.0.0"), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(ipaddr, 0, wxGROW);
+	
+	// Port
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Port:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	port = new wxTextCtrl(settingsPanel, wxID_ANY, _("12000"), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(port, 0, wxGROW);
+	
+	// Port
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Password:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	passwd = new wxTextCtrl(settingsPanel, wxID_ANY, _(""), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(passwd, 0, wxGROW);
+
+	// playercount
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Slots:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	slots = new wxTextCtrl(settingsPanel, wxID_ANY, _("16"), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(slots, 0, wxGROW);
+
+	// terrain
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("Terrain:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+	terrain = new wxTextCtrl(settingsPanel, wxID_ANY, _("any"), wxDefaultPosition, wxDefaultSize);
+	hsizer->Add(terrain, 0, wxGROW);
+
+	// debuglevel
+	dText = new wxStaticText(settingsPanel, wxID_ANY, _("LogLevel:"), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	hsizer->Add(dText, 0, wxGROW);
+
+	wxString choices_log[6];
+	choices_log[0] = _("STACK");
+	choices_log[1] = _("DEBUG");
+	choices_log[2] = _("VERBOSE");
+	choices_log[3] = _("INFO");
+	choices_log[4] = _("WARN");
+	choices_log[5] = _("ERRROR");
+	logmode = new wxComboBox(settingsPanel, wxID_ANY, _("INFO"), wxDefaultPosition, wxDefaultSize, 6, choices_log, wxCB_DROPDOWN|wxCB_READONLY);
+	hsizer->Add(logmode, 0, wxGROW);
+
+	// some buttons
+	startBtn = new wxButton(settingsPanel, btn_start, _("START"));
 	hsizer->Add(startBtn, 0, wxGROW);
 
-	stopBtn = new wxButton(this, btn_stop, _("STOP"));
+	stopBtn = new wxButton(settingsPanel, btn_stop, _("STOP"));
 	stopBtn->Disable();
 	hsizer->Add(stopBtn, 0, wxGROW);
 
-	exitBtn = new wxButton(this, btn_exit, _("EXIT"));
-	hsizer->Add(exitBtn, 0, wxGROW);
+	settingsSizer->Add(hsizer, 0, wxGROW);
+	settingsPanel->SetSizer(settingsSizer);
+	nbook->AddPage(settingsPanel, _("Settings"), true);
 
-	mainsizer->Add(hsizer, 0, wxGROW);
+	///// Log
+	logPanel=new wxPanel(nbook, wxID_ANY);
+	wxSizer *logSizer = new wxBoxSizer(wxVERTICAL);
+	txtConsole = new wxTextCtrl(logPanel, wxID_ANY, _(""), wxDefaultPosition, wxDefaultSize, wxNO_BORDER|wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH|wxTE_RICH2|wxTE_DONTWRAP);
+	logSizer->Add(txtConsole, 1, wxGROW);
+	logPanel->SetSizer(logSizer);
+	nbook->AddPage(logPanel, _("Log"), false);
+
+	exitBtn = new wxButton(this, btn_exit, _("EXIT"));
+	mainsizer->Add(exitBtn, 0, wxGROW);
+	
+
 
 	// add the logger callback
 	Logger::setCallback(&pureLogCallback);
@@ -209,7 +285,8 @@ void MyDialog::OnBtnStart(wxCommandEvent& event)
 {
 	startBtn->Disable();
 	stopBtn->Enable();
-	Sequencer::initilize();
+	nbook->SetSelection(1);
+	startServer();
 
 }
 
@@ -217,6 +294,7 @@ void MyDialog::OnBtnStop(wxCommandEvent& event)
 {
 	startBtn->Enable();
 	stopBtn->Disable();
+	stopServer();
 }
 
 void MyDialog::OnBtnExit(wxCommandEvent& event)
@@ -228,16 +306,40 @@ void MyDialog::OnBtnExit(wxCommandEvent& event)
 
 int MyDialog::startServer()
 {
-	// det default verbose levels
-	Logger::setLogLevel(LOGTYPE_DISPLAY, LOG_INFO);
+	// set default verbose levels
+	loglevel = LOG_INFO;
+	if(logmode->GetValue() == _("STACK"))   loglevel = LOG_STACK;
+	if(logmode->GetValue() == _("DEBUG"))   loglevel = LOG_DEBUG;
+	if(logmode->GetValue() == _("VERBOSE")) loglevel = LOG_VERBOSE;
+	if(logmode->GetValue() == _("INFO"))    loglevel = LOG_INFO;
+	if(logmode->GetValue() == _("WARN"))    loglevel = LOG_WARN;
+	if(logmode->GetValue() == _("ERROR"))   loglevel = LOG_ERROR;
+	
 	Logger::setLogLevel(LOGTYPE_FILE, LOG_VERBOSE);
 	Logger::setFlushLevel(LOG_ERROR);
 	Logger::setOutputFile("server.log");
 
-	if(smode->GetSelection() == 0)
+	if(smode->GetValue() == _("LAN"))
 		Config::setServerMode(SERVER_LAN);
-	else if(smode->GetSelection() == 1)
+	else
 		Config::setServerMode(SERVER_INET);
+
+	if(ipaddr->GetValue() != _("0.0.0.0"))
+		Config::setIPAddr(conv(ipaddr->GetValue()));
+	
+	unsigned long portNum=12000;
+	port->GetValue().ToULong(&portNum);
+	Config::setListenPort(portNum);
+
+	unsigned long slotNum=16;
+	slots->GetValue().ToULong(&slotNum);
+	Config::setMaxClients(slotNum);
+
+	if(!passwd->GetValue().IsEmpty())
+		Config::setPublicPass(conv(passwd->GetValue()));
+
+	Config::setServerName(conv(sname->GetValue()));
+	Config::setTerrain(conv(terrain->GetValue()));
 		
 	if( !Config::checkConfig() )
 		return 1;
@@ -246,8 +348,6 @@ int MyDialog::startServer()
 
 	if(Config::getServerMode() == SERVER_INET)
 		Sequencer::notifyRoutine(); 
-
-	Sequencer::cleanUp();
 	return 0;
 }
 

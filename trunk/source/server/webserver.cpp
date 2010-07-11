be6da2b7d9276d56101a2cd89ebbfe7f06e86185
rorthomas <rorthomas@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2010-07-11 15:19:19 +0000

    webserver working better with YUI/json fixed mongoose bug

--- trunk/source/server/webserver.cpp
+++ trunk/source/server/webserver.cpp
@@ -94,8 +94,9 @@ int getPlayerColour(int num, char *res)
 	return 0;
 }
 
-int formatBytes(double bytes, char *res, bool persec=false)
+std::string formatBytes(double bytes, bool persec=false)
 {
+	char res[256]="";
 	if(bytes < 1)
 		sprintf(res, "0");
 	else if(bytes <= 1024)
@@ -109,8 +110,8 @@ int formatBytes(double bytes, char *res, bool persec=false)
 	//else if(bytes > 1099511627776)
 	//	sprintf(res, "%0.2f TB", bytes / 1024.0f / 1024.0f / 1024.0f / 1024.0f);
 	if( !(bytes < 1) && persec)
-		strcat(res, " /s");
-	return 0;
+		strcat(res, "/s");
+	return std::string(res);
 }
 
 static ctemplate::TemplateDictionary *getTemplateDict(std::string title)
@@ -134,7 +135,7 @@ static void renderTemplate(ctemplate::TemplateDictionary *dict, struct mg_connec
 	std::string output;
 	ctemplate::ExpandTemplate(templatefile, ctemplate::DO_NOT_STRIP, dict, &output);
 
-	mg_printf(conn, "%s", output.c_str());
+	mg_write(conn, output.c_str(), output.size());
 
 	delete(dict);
 	dict=0;
@@ -149,30 +150,220 @@ static void show_index(struct mg_connection *conn, const struct mg_request_info 
 	renderTemplate(dict, conn, "webserver/templates/overview.html");
 }
 
+#define ranrange(a, b) (int)((a) + rand()/(RAND_MAX + 1.0) * ((b) - (a) + 1))
+static void data_stats_traffic(struct mg_connection *conn, const struct mg_request_info *request_info, void *data)
+{
+	Json::Value root;   // will contains the root value after parsing.
+	Json::Value results;
+	for(int i=0;i<100;i++)
+	{
+		double val = ranrange(0,100);
+		Json::Value result;
+		result["Name"]  = i;
+		result["Value"] = (int)val;
+		results.append(result);
+	}
+	root["Traffic"] = results;
+	Json::StyledWriter writer;
+	std::string output = writer.write( root );
 
-static void data_players(struct mg_connection *conn, const struct mg_request_info *request_info, void *data)
+	mg_write(conn, output.c_str(), output.size());
+}
+
+static Json::Value getConfValue(std::string name, std::string value)
+{
+	Json::Value result;
+	result["name"]  = name;
+	result["value"] = value;
+	return result;
+}
+
+static Json::Value getConfValue(std::string name, int value)
+{
+	Json::Value result;
+	result["name"]  = name;
+	result["value"] = value;
+	return result;
+}
+
+static void data_configuration(struct mg_connection *conn, const struct mg_request_info *request_info, void *data)
 {
 	static int i=0;
 	Json::Value root;   // will contains the root value after parsing.
-	root["ResultSet"]["totalResultsAvailable"] = 4;
-	root["ResultSet"]["totalResultsReturned"] = 4;
-	root["ResultSet"]["firstResultPosition"] = 1;
 	Json::Value results;
-	for(int j=0;j<10;j++,i++)
+	results.append(getConfValue("Max Clients", Config::getMaxClients()));
+	results.append(getConfValue("Server Name", Config::getServerName()));
+	results.append(getConfValue("Terrain Name", Config::getTerrainName()));
+	results.append(getConfValue("Password Protected", Config::getPublicPassword().empty()?"No":"Yes"));
+	results.append(getConfValue("IP Address", Config::getIPAddr()=="0.0.0.0"?"0.0.0.0 (Any)":Config::getIPAddr()));
+	results.append(getConfValue("Listening Port", Config::getListenPort()));
+	results.append(getConfValue("Protocol Version", std::string(RORNET_VERSION)));
+
+	bool advertised = Sequencer::getNotifier()->getAdvertised();
+	std::string serverMode = "AUTO";
+	if(Config::getServerMode() == SERVER_LAN)
+		serverMode = "LAN";
+	else if(Config::getServerMode() == SERVER_INET && advertised)
+		serverMode = "Public, Internet";
+	else if(Config::getServerMode() == SERVER_INET && !advertised)
+		serverMode = "Private, Internet";
+
+	results.append(getConfValue("Advertized on Master Server", advertised?"Yes":"No"));
+	results.append(getConfValue("Server Mode", serverMode));
+	
+	if(Config::getServerMode() == SERVER_INET && advertised)
 	{
-		Json::Value result;
-		char tmp[255];
-		sprintf(tmp, "%s%d", "test", i);
-		result["name"] = std::string(tmp);
-		
-		results.append(result);
+		int trustlevel = Sequencer::getNotifier()->getTrustLevel();
+		results.append(getConfValue("Server Trust Level", trustlevel));
 	}
+
+	results.append(getConfValue("Print Console Stats", Config::getPrintStats()?"Yes":"No"));
+
+#ifdef WITH_ANGELSCRIPT
+	results.append(getConfValue("Scripting support", "Yes"));
+	results.append(getConfValue("Scripting enabled", Config::getEnableScripting()?"Yes":"No"));
+	if(Config::getEnableScripting())
+		results.append(getConfValue("Script Name in use", Config::getScriptName()));
+#else // WITH_ANGELSCRIPT
+	results.append(getConfValue("Scripting support", "No"));
+#endif // WITH_ANGELSCRIPT
+
+	results.append(getConfValue("Webserver Port", Config::getWebserverPort()));
+
 	root["ResultSet"]["Result"] = results;
+	root["ResultSet"]["totalResultsAvailable"] = results.size();
+	root["ResultSet"]["totalResultsReturned"] = results.size();
+	root["ResultSet"]["firstResultPosition"] = 1;
 
 	Json::StyledWriter writer;
 	std::string output = writer.write( root );
 
-	mg_printf(conn, "%s", output.c_str());
+	mg_write(conn, output.c_str(), output.size());
+}
+
+static void data_players(struct mg_connection *conn, const struct mg_request_info *request_info, void *data)
+{
+	Json::Value rows;
+
+	std::vector<client_t> clients = Sequencer::getClients();
+	int row_counter=0;
+	for(std::vector<client_t>::iterator it = clients.begin(); it != clients.end(); it++,row_counter++)
+	{
+		Json::Value row;
+
+		if (it->status == FREE)
+		{
+			row["slot"] = it->slotnum;
+			row["status"] = "FREE";
+			rows.append(row);
+		} else if (it->status == BUSY)
+		{
+			row["slot"] = it->slotnum;
+			row["status"] = "BUSY";
+			rows.append(row);
+		} else if (it->status == USED)
+		{
+			// some auth identifiers
+			std::string authst = "none";
+			if(it->authstate & AUTH_BANNED) authst = "banned";
+			if(it->authstate & AUTH_BOT)    authst = "bot";
+			if(it->authstate & AUTH_RANKED) authst = "ranked";
+			if(it->authstate & AUTH_MOD)    authst = "moderator";
+			if(it->authstate & AUTH_ADMIN)  authst = "admin";
+
+			char playerColour[128] = "";
+			getPlayerColour(it->colournumber, playerColour);
+
+			row["slot"]     = it->slotnum;
+			row["status"]   = "USED";
+			row["uid"]      = it->uid;
+			row["ip"]       = std::string(it->ip_addr);
+			row["name"]     = std::string(it->nickname);
+			row["auth"]     = authst;
+
+			// get traffic stats for all streams
+			double bw_in=0, bw_out=0, bw_in_rate=0, bw_out_rate=0;
+			double bw_drop_in=0, bw_drop_out=0, bw_drop_in_rate=0, bw_drop_out_rate=0;
+			// tit = traffic iterator, not what you might think ...
+			for(std::map<unsigned int, stream_traffic_t>::iterator tit = it->streams_traffic.begin(); tit != it->streams_traffic.end(); tit++)
+			{
+				bw_in            += tit->second.bandwidthIncoming;
+				bw_out           += tit->second.bandwidthOutgoing;
+				bw_in_rate       += tit->second.bandwidthIncomingRate;
+				bw_out_rate      += tit->second.bandwidthOutgoingRate;
+				
+				bw_drop_in       += tit->second.bandwidthDropIncoming;
+				bw_drop_out      += tit->second.bandwidthDropOutgoing;
+				bw_drop_in_rate  += tit->second.bandwidthDropIncomingRate;
+				bw_drop_out_rate += tit->second.bandwidthDropOutgoingRate;
+			}
+
+			// traffic things
+			row["bw_normal_sum_up"]      = formatBytes(bw_in);
+			row["bw_normal_sum_down"]    = formatBytes(bw_out);
+			row["bw_normal_rate_up"]     = formatBytes(bw_in_rate, true);
+			row["bw_normal_rate_down"]   = formatBytes(bw_out_rate, true);
+
+			row["bw_drop_sum_up"]        = formatBytes(bw_drop_in);
+			row["bw_drop_sum_down"]      = formatBytes(bw_drop_out);
+			row["bw_drop_rate_up"]       = formatBytes(bw_drop_in_rate, true);
+			row["bw_drop_rate_down"]     = formatBytes(bw_drop_out_rate, true);
+
+			rows.append(row);
+			
+			// now add the streams themself to the table
+			for(std::map<unsigned int, stream_register_t>::iterator sit = it->streams.begin(); sit != it->streams.end(); sit++)
+			{
+				Json::Value trow;
+
+				std::string typeStr = "unkown";
+				if(sit->second.type == 0) typeStr = "Vehicle";
+				if(sit->second.type == 1) typeStr = "Character";
+				if(sit->second.type == 2) typeStr = "AI Traffic";
+				if(sit->second.type == 3) typeStr = "Chat";
+
+				stream_traffic_t *traf = 0;
+				if(it->streams_traffic.find(sit->first) != it->streams_traffic.end())
+					traf = &(it->streams_traffic.find(sit->first)->second);
+
+				char uidtmp[128] = "";
+				sprintf(uidtmp, "%03d:%02d", it->uid, sit->first);
+
+				trow["slot"]     = "";
+				trow["status"]   = sit->second.status;
+				trow["uid"]      = std::string(uidtmp);
+				trow["ip"]       = typeStr;
+				trow["name"]     = std::string(sit->second.name);
+				trow["auth"]     = "";
+				if(traf)
+				{
+					trow["bw_normal_sum_up"]      = formatBytes(traf->bandwidthIncoming);
+					trow["bw_normal_sum_down"]    = formatBytes(traf->bandwidthOutgoing);
+					trow["bw_normal_rate_up"]     = formatBytes(traf->bandwidthIncomingRate , true);
+					trow["bw_normal_rate_down"]   = formatBytes(traf->bandwidthOutgoingRate, true);
+
+					trow["bw_drop_sum_up"]        = formatBytes(traf->bandwidthDropIncoming);
+					trow["bw_drop_sum_down"]      = formatBytes(traf->bandwidthDropOutgoing);
+					trow["bw_drop_rate_up"]       = formatBytes(traf->bandwidthDropIncomingRate, true);
+					trow["bw_drop_rate_down"]     = formatBytes(traf->bandwidthDropOutgoingRate, true);
+				}
+				rows.append(trow);
+			}
+
+		}
+
+	}
+
+	Json::Value root;   // will contains the root value after parsing.
+	root["ResultSet"]["totalResultsAvailable"] = row_counter;
+	root["ResultSet"]["totalResultsReturned"]  = row_counter;
+	root["ResultSet"]["firstResultPosition"]   = 1;
+	root["ResultSet"]["Result"] = rows;
+
+	Json::StyledWriter writer;
+	std::string output = writer.write( root );
+
+	mg_write(conn, output.c_str(), output.size());
 }
 
 static void show_404(struct mg_connection *conn, const struct mg_request_info *request_info, void *user_data)
@@ -215,6 +406,8 @@ int startWebserver(int port)
 	/* Register an index page under two URIs */
 	mg_set_uri_callback(ctx, "/", &show_index, NULL);
 	mg_set_uri_callback(ctx, "/data/players/", &data_players, NULL);
+	mg_set_uri_callback(ctx, "/data/stats/traffic", &data_stats_traffic, NULL);
+	mg_set_uri_callback(ctx, "/data/configuration/", &data_configuration, NULL);
 
 	mg_set_error_callback(ctx, 404, show_404, NULL);
 	return 0;

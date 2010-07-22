f460db48d6362013d2e4498307c963a3c106344e
rorthomas <rorthomas@7bf4d4c7-3b9c-4447-a9e5-7c6d8903caca>
2010-07-22 13:18:14 +0000

    fixes #358

--- source/server/listener.cpp
+++ source/server/listener.cpp
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdexcept>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 
 #ifdef __GNUC__
 #include <stdlib.h>
